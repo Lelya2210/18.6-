2cc4a8b83a3f32d893e82e28ebaef6e5af66c031
Lelya2210 <117176572+Lelya2210@users.noreply.github.com>
2022-12-11 00:21:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,16 +19,16 @@ def values(message: telebot.types.Message):
     bot.reply_to(message, text)
 
 @bot.message_handler(content_types =  ['text'])
-def convert(message: telebot.types.Message):
+def get_price(message: telebot.types.Message):
     try:
         values = message.text.split(' ')
 
         if len(values) != 3:
-            raise ConvertionException('Слишком много параметров. Необходимо ввести только 3 параметра')
+            raise APIException('Слишком много параметров. Необходимо ввести только 3 параметра')
 
         quote, base, amount = values
         total_base = CurrencyConverter.convert(quote, base, amount)
-    except ConvertionException as e:
+    except APIException as e:
         bot.reply_to(message, f'Ошибка пользователя.\n{e}')
     except Exception as e:
         bot.reply_to(message, f'Не удалось обработать команду\n{e}')
